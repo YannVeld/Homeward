8575d4176f693a9a860f8d269f16a5b3df0fbb38
Yann in 't Veld <yannintveld@gmail.com>
2023-09-30 10:16:25 +0200

    Pulled pickupable into its own class

--- src/Instances/Item.lua
+++ src/Instances/Item.lua
@@ -1,15 +1,14 @@
+require("src/Instances/Pickupable")
 
 Item = Class{
-    __includes = {Instance},
+    __includes = {Pickupable},
 
-    init = function(self, position, sprite, cellsWide, cellsHig, pickupManager)
-        Instance.init(self)
+    init = function(self, position, sprite, cellsWide, cellsHigh, pickupManager)
+        Pickupable.init(self, position, sprite:getWidth(), sprite:getHeight(), pickupManager)
 
-        self.position = position
         self.sprite = sprite
         self.cellsWide = cellsWide
         self.cellsHigh = cellsHigh
-        self.pickupManager = pickupManager
     end,
 
     update = function(self, dt)
@@ -21,48 +20,5 @@ Item = Class{
 
     draw = function(self)
         love.graphics.draw(self.sprite, self.position.x - self.sprite:getWidth()/2, self.position.y - self.sprite:getHeight() / 2)
-    end,
-
-    posIsOnItem = function(self, x, y)
-        --local mousex, mousey = Push:toGame(love.mouse.getPosition())
-
-        local leftedge = self.position.x - self.sprite:getWidth()/2
-        local topedge = self.position.y - self.sprite:getHeight() / 2
-        local rightedge = leftedge + self.sprite:getWidth()
-        local bottomedge = topedge + self.sprite:getHeight()
-
-        if x < leftedge or x > rightedge then
-            return false
-        end
-        if y < topedge or y > bottomedge then
-            return false
-        end
-        return true
-    end,
-
-    mousereleased = function(self, x, y, button, istouch, presses)
-        local mousex, mousey = Push:toGame(x, y)
-
-        if not self:posIsOnItem(mousex, mousey) then
-            return
-        end
-
-        if button == 1 then
-            self:pickupOrDropMe()
-        end
-
-    end,
-
-    pickupOrDropMe = function(self)
-        if pickupManager:isHolding(self) then
-            pickupManager:dropItem()
-            return
-        end
-
-        pickupManager:pickupItem(self)
-    end,
-
-    isPickedUp = function(self)
-        return pickupManager:isHolding(self)
     end,
 }
---
+++ src/Instances/Pickupable.lua
@@ -0,0 +1,62 @@
+Pickupable = Class{
+    __includes = {Instance},
+
+    init = function(self, position, width, height, pickupManager)
+        Instance.init(self)
+
+        self.position = position
+        self.width = width
+        self.height = height
+        self.pickupManager = pickupManager
+    end,
+
+    update = function(self, dt)
+        if self:isPickedUp() then
+            local mousex, mousey = Push:toGame(love.mouse.getPosition())
+            self.position = Vector(mousex, mousey)
+        end
+    end,
+
+    posIsOnItem = function(self, x, y)
+        --local mousex, mousey = Push:toGame(love.mouse.getPosition())
+
+        local leftedge = self.position.x - self.width/2
+        local topedge = self.position.y - self.height/2
+        local rightedge = leftedge + self.width
+        local bottomedge = topedge + self.height
+
+        if x < leftedge or x > rightedge then
+            return false
+        end
+        if y < topedge or y > bottomedge then
+            return false
+        end
+        return true
+    end,
+
+    mousereleased = function(self, x, y, button, istouch, presses)
+        local mousex, mousey = Push:toGame(x, y)
+
+        if not self:posIsOnItem(mousex, mousey) then
+            return
+        end
+
+        if button == 1 then
+            self:pickupOrDropMe()
+        end
+
+    end,
+
+    pickupOrDropMe = function(self)
+        if pickupManager:isHolding(self) then
+            pickupManager:dropItem()
+            return
+        end
+
+        pickupManager:pickupItem(self)
+    end,
+
+    isPickedUp = function(self)
+        return pickupManager:isHolding(self)
+    end,
+}
