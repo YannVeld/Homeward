6a898430bd6bf5069f59f3c18e64c2df056b5476
Yann in 't Veld <yannintveld@gmail.com>
2023-09-30 17:28:46 +0200

    Added hex to rgb function

--- Packages/UI/Colors.lua
+++ Packages/UI/Colors.lua
@@ -185,3 +185,7 @@ end
 Colors.RGB = function (name)
     return Colors[name][1],Colors[name][2],Colors[name][3]
 end
+
+Colors.hexToRGB = function(hex, value)
+    return {tonumber(string.sub(hex, 2, 3), 16)/256, tonumber(string.sub(hex, 4, 5), 16)/256, tonumber(string.sub(hex, 6, 7), 16)/256, value or 1}
+end
