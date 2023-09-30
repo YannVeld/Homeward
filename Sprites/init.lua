d20d75f026bc0779a07911e4b8799f37090b0641
Yann in 't Veld <yannintveld@gmail.com>
2023-09-30 15:47:50 +0200

    Implemented automatic loading of sprites

--- Sprites/init.lua
+++ Sprites/init.lua
@@ -9,3 +9,17 @@ Sprites.redSquare = love.graphics.newImage('Sprites/Placeholder/redSquare.png')
 Sprites.yellowCircle = love.graphics.newImage('Sprites/Placeholder/yellowCircle.png')
 
 
+-- Load all sprites in base folder
+local spriteDir = "Sprites"
+local filenames = love.filesystem.getDirectoryItems(spriteDir)
+
+for i,file in ipairs(filenames) do
+    local splitted = Lume.split(file, ".")
+    local name = splitted[1]
+    local extension = splitted[2]
+
+    if (extension == "png") then
+        local sprite = love.graphics.newImage(spriteDir .. "/" .. file)
+        Sprites[name] = sprite
+    end
+end
