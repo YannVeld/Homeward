654d9b10d47bde6b0e03650c4d3be914bae31499
Yann in 't Veld <yannintveld@gmail.com>
2023-09-30 17:14:07 +0200

    Removed example pong game

--- src/GameStates/thegame.lua
+++ src/GameStates/thegame.lua
@@ -1,7 +1,3 @@
-require("src/Instances/scoreManager")
-require("src/Instances/player")
-require("src/Instances/ball")
-
 require("src/Instances/Item")
 require("src/Instances/itemPickupManager")
 
@@ -18,19 +14,7 @@ function gamestates.thegame:enter()
     local cellWidth = 16
     local cellHeight = 16
     myGrid = Grid(pos, width, height, cellWidth, cellHeight)
-
-    --local playerDistFromEdge = 30
-
-    --local ypos = Push:getHeight() / 2
-    --local player1pos = Vector(playerDistFromEdge, ypos)
-    --player1 = Player(player1pos, "move_pl1", 1)
-    --local player2pos = Vector(Push:getWidth() - playerDistFromEdge, ypos)
-    --player2 = Player(player2pos, "move_pl2", 2)
-
-    --ball = Ball(player1, player2)
-
-    --scoreManager = ScoreManager()
-
+    
     pickupManager = ItemPickupManager()
 
     anItem = NewSwordItem(Vector(16, 16), pickupManager, myGrid)
@@ -38,23 +22,17 @@ function gamestates.thegame:enter()
 end
 
 function gamestates.thegame:update(dt)
-    --local mousex, mousey = Push:toGame(love.mouse.getPosition())
-
-    --local i,j = myGrid:getGridIndex(mousex, mousey)
-    --if myGrid:isGridIndex(i,j) then
-    --    myGrid:overwrite(i,j,true)
-    --end
 
 end
 
 function gamestates.thegame:keyreleased(key, code)
     if key == 'escape' then
-        Gamestate.switch(gamestates.menu)
+        love.event.quit()
     end
 
-    if key == 'space' then
-        myGrid:clearAll()
-    end
+    --if key == 'space' then
+    --    myGrid:clearAll()
+    --end
 end
 
 function gamestates.thegame:draw()
