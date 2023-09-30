fa8fc9d885ca9b2399d6e8643b17bf3d606759a6
Yann in 't Veld <yannintveld@gmail.com>
2023-09-30 21:15:09 +0200

    Continue button

--- src/Instances/sceneManager.lua
+++ src/Instances/sceneManager.lua
@@ -6,10 +6,12 @@ SceneManager = Class{
         Instance.init(self)
 
         self.curScene = initialScene
-
         self.pickupManager = pickupManager
         self.grid = grid
         self.itemStorage = itemStorage
+
+        self.continueButtonPos = Vector(49, 95)
+        self.continueButton = nil
 
         Signal.register('conversionsDone', function(id) self:afterConversion(id) end)
         self:setupScene()
@@ -27,11 +29,28 @@ SceneManager = Class{
     end,
 
     afterConversion = function(self, id)
+        local buttonwidth = Sprites.ContinueButton:getWidth()
+        local buttonheight = Sprites.ContinueButton:getHeight()
+
+        self.continueButton = Button(self.continueButtonPos, buttonwidth, buttonheight)
+        self.continueButton:setText("Continue", {color=textColor, ha="center", va="center"})
+        self.continueButton:setOnButtonReleased(function() self:continueToNextScene() end)
+        self.continueButton:setBackgroundSprites(Sprites.ContinueButton, Sprites.ContinueButtonHover, Sprites.ContinueButton, false)
+        self.continueButton:setDrawLayer(DrawLayers.Normal)
+    end,
+
+    continueToNextScene = function(self)
+        self.itemStorage.item:destroy()
+
         if id == 1 then
             self.curScene = self.curScene.followupScene1
         else
             self.curScene = self.curScene.followupScene2
         end
+       
+        self.continueButton:destroy()
+        self.continueButton = nil
+
         self:setupScene()
     end,
 }
